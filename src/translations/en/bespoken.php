f9782281a57078262d5bd1a83c6e7f212c059da4
John F Morton <morton@jmx2.com>
2024-09-06 16:37:09 -0400

    Text updates

--- src/translations/en/bespoken.php
+++ src/translations/en/bespoken.php
@@ -10,7 +10,7 @@ return [
     'Voice ID' => 'Voice ID',
     'Voice name' => 'Voice name',
     'ElevenLabs API Key instructions' => 'Found in Profile Settings in the ElevenLabs control panel. Can be an environment variable.',
-    'Add voices instructions' => 'Add voices that can be used to create text. See the <a href="https://elevenlabs.io/app/voice-lab">Voice Lab</a> section in the ElevenLabs control panel to configure the voices you\'d like to use. You may use any of the voices in the ElevenLabs library or create your own voice. You will need the <b>Voice ID</b>.',
+    'Add voices instructions' => 'Add voices below that can be used to create text in Bespoken. See the <a href="https://elevenlabs.io/app/voice-lab">Voice Lab</a> section in the ElevenLabs control panel to determine the voices you\'d like to use. You may use any of the voices in the ElevenLabs library or create your own voice. You will need the <b>Voice ID</b>.',
     'Voice options' => 'Choose the voices for this field',
     'No voice options available for plugin' => '🛑 The Bespoken plugin has no voice options available. Add at least one voice to the plugin in the settings page.',
     'No voice options available for field' => '🛑 The Bespoken plugin has no voice options available for this field. Choose at least one voice in the field set up screen.',
@@ -18,7 +18,7 @@ return [
     'Voice options instructions' => 'Below are all of the voices available in the plugin. Select the voices you would like to use to read the source field aloud.',
     'Voice field select label' => 'Select a voice',
     'Voice field select instructions' => 'Choose the voice that will be used to create the audio file.',
-    'Voice model instructions' => "ElevenLabs has five voice model options. <b>Turbo v2.5</b> is the latest model. For more information read <a href='https://elevenlabs.io/docs/speech-synthesis/models'>ElevenLabs Models documentation</a> and the article, <a href='https://help.elevenlabs.io/hc/en-us/articles/17883183930129-What-models-do-you-offer-and-what-is-the-difference-between-them'>What models do you offer and what is the difference between them?</a>",
+    'Voice model instructions' => "Choose the voice model to use with Bespoken. ElevenLabs has five voice model options. <b>Turbo v2.5</b> is the latest model. The default is <b>Multilingual v2</b>. For more information read <a href='https://elevenlabs.io/docs/speech-synthesis/models'>ElevenLabs Models documentation</a> and the article, <a href='https://help.elevenlabs.io/hc/en-us/articles/17883183930129-What-models-do-you-offer-and-what-is-the-difference-between-them'><i>What models do you offer and what is the difference between them?</i></a>",
     'Voice model intro' => '<p>Visit the <a href="https://elevenlabs.io/app/voice-lab">Voice Lab</a> section in the ElevenLabs control panel to configure the voices you\'d like to use. You may use any of the voices in the ElevenLabs library or create your own voice. You will need the <b>Voice ID</b>.</p>',
     'Voice lab instructions' => '<p>All values below should be left at the defaults under most circumstances. For more information, read the <a
             href="https://elevenlabs.io/docs/speech-synthesis/voice-settings" target="_blank">ElevenLabs documentation for voice settings.</a></p>',

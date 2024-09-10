b89c0d111084efa4904f6a0201d368b75fca46bc
John F Morton <morton@jmx2.com>
2024-09-10 16:15:18 -0400

    Fixed validator with updated values

--- src/validators/BespokenPronuciationValidator.php
+++ src/validators/BespokenPronuciationValidator.php
@@ -23,17 +23,17 @@ class BespokenPronuciationValidator extends Validator
             }
 
             // Check if 'voice' and 'voiceId' keys exist
-            if (!isset($item['voice']) || !isset($item['voiceId'])) {
+            if (!isset($item['word']) || !isset($item['pronunciation'])) {
                 return [Craft::t('app', 'Each item must contain "word" and "pronunciation" keys.'), []];
             }
 
             // Check if 'voice' and 'voiceId' values are strings
-            if (!is_string($item['voice']) || !is_string($item['voiceId'])) {
+            if (!is_string($item['word']) || !is_string($item['pronunciation'])) {
                 return [Craft::t('app', '"word" and "pronunciation" must be strings.'), []];
             }
 
             // Check if 'voice' and 'voiceId' values are not empty
-            if (empty($item['voice']) || empty($item['voiceId'])) {
+            if (empty($item['word']) || empty($item['pronunciation'])) {
                 return [Craft::t('app', '"Word" and "Pronunciation" cannot be empty.'), []];
             }
         }

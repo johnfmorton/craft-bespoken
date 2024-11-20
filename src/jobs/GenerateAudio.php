badf5afd56bd9020bae5d37ebdc5c09c0216d0aa
John F Morton <morton@jmx2.com>
2024-11-20 13:34:36 -0500

    Get URL for test from Craft CMS

--- src/jobs/GenerateAudio.php
+++ src/jobs/GenerateAudio.php
@@ -78,8 +78,10 @@ class GenerateAudio extends BaseJob
         // add a pause to simulate a long-running process
         sleep($this->sleepValue);
 
+        $publicUrlFromCraft = Craft::$app->sites->currentSite->baseUrl;
+
         // download a test file with CURL - this must be a publicly accessible file on the same URL as the site for testing
-        $url = 'https://craftquest-demo.ddev.site/test.mp3';
+        $url = $publicUrlFromCraft. 'test.mp3';
         $tempDir = $this->getTempDirectory();
         $timestamp = time();
         $tempFilePath = $tempDir . '/test' . $timestamp . '.mp3';
